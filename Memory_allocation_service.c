e8565982343bf9c671fbdbdbb88abdee9e0173d2
Hodaya Marciano <hodayamar2000@gmail.com>
2018-11-24 12:58:56 -0800

    Finish the first version of all the functions, without any tests yet.

--- Memory_allocation_service.c
+++ Memory_allocation_service.c
@@ -1,12 +1,17 @@
 #include "Memory_allocation_service.h"
 
-struct MemoryAllocator{
+#define AVAILABLE 0
+#define UNAVAILABLE 1
+
+struct MemoryAllocator
+{
 
     void* memory_ptr;
     size_t size_of_memory;
 };
 
-MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size){
+MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size)
+{
 
     MemoryAllocator *p_MemoryAllocator = malloc(sizeof(MemoryAllocator));
 
@@ -17,29 +22,108 @@ MemoryAllocator* MemoryAllocator_init(void* memoryPool, size_t size){
     p_MemoryAllocator-> size_of_memory = size;
     *((size_t*)p_MemoryAllocator->memory_ptr) = p_MemoryAllocator->size_of_memory;
     *((size_t*)p_MemoryAllocator->memory_ptr)|= 1 << 0;
-
-
+    printf("%lu", *((size_t*)p_MemoryAllocator->memory_ptr));
     return p_MemoryAllocator;
 }
 
-void* MemoryAllocator_allocate(MemoryAllocator* allocator, size_t size){
+void* MemoryAllocator_allocate(MemoryAllocator* allocator, size_t size)
+{
 
+    void* next_block = allocator->memory_ptr;
+    size_t index = *((size_t*)next_block);
+    size_t pow_of_two = 1;
+    size_t not_located = 1;
 
-return NULL;
+    while(pow_of_two < size)
+        pow_of_two *= 2;
+
+    size = pow_of_two;
+
+    while(not_located)
+    {
+        if(*((size_t*)next_block) & 1)
+        {
+            index = *((size_t*)next_block);
+            if((size_t*)allocator->memory_ptr - (index + (size_t*)next_block) > allocator->size_of_memory)
+                return NULL;
+            next_block = index + (size_t*)next_block;
+
+        }
+
+        else if(*((size_t*)next_block) >= size)
+        {
+            *((size_t*)allocator->memory_ptr + (size_t)((size_t*)allocator->memory_ptr - (size_t*)next_block) + size + 1) =
+                    *((size_t*)next_block) - size;
+
+            *((size_t*)allocator->memory_ptr + (size_t)((size_t*)allocator->memory_ptr - (size_t*)next_block)) = size + 1;
+
+            not_located = 0;
+        }
+        else if((*((size_t*)next_block + *((size_t*)next_block) + 1) & 0) == 0)
+        {
+            *((size_t*)allocator->memory_ptr + (size_t)((size_t*)allocator->memory_ptr - (size_t*)next_block)) =
+                    *((size_t*)next_block) + *((size_t*)next_block + *((size_t*)next_block));
+
+        }
+
+    }
+
+    return next_block;
 
 }
 
 /* Return number of still allocated blocks */
 size_t MemoryAllocator_free(MemoryAllocator* allocator, void* ptr){
-    return 0;
 
+    size_t end_of_array = 1;
+    void* next_block = allocator->memory_ptr;
+    size_t index = *((size_t*)next_block);
+    size_t still_allocated_blocks = 0;
+
+    while(end_of_array)
+    {
+        if(*((size_t*)next_block) & 1)
+            still_allocated_blocks += 1;
+
+        index = *((size_t*)next_block);
+        if((size_t*)allocator->memory_ptr - (index + (size_t*)next_block) <= allocator->size_of_memory)
+            next_block = index + (size_t*)next_block;
+        else
+            end_of_array = 0;
+    }
+
+    return still_allocated_blocks;
 
 }
 
 /* Return the size of largest free block */
 size_t MemoryAllocator_optimize(MemoryAllocator* allocator){
 
-    return 0;
+    MemoryAllocator_allocate(allocator, allocator->size_of_memory);
+
+    size_t end_of_array = 1;
+    void* next_block = allocator->memory_ptr;
+    size_t index = *((size_t*)next_block);
+    size_t largest_free_block = *((size_t*)next_block);
+
+    while(end_of_array)
+    {
+        index = *((size_t*)next_block);
+
+        if(index >= largest_free_block)
+            largest_free_block = *((size_t*)next_block;
+
+
+        if((size_t*)allocator->memory_ptr - (index + (size_t*)next_block) <= allocator->size_of_memory)
+            next_block = index + (size_t*)next_block;
+        else
+            end_of_array = 0;
+
+
+
+    }
+
+    return still_allocated_blocks;
 
 }
 
